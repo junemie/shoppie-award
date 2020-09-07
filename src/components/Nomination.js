416438070c979886f7719d42d48160ee8b42a046
June Hosoya <junehosoya@gmail.com>
2020-09-07 00:41:52 -0700

    Fix variable name in Nomination component

--- src/components/Nomination.js
+++ src/components/Nomination.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Nominations = (props) => {
-  const { movies, nominations, nominationHandler } = props;
+  const { nominations, nominationHandler } = props;
 
   return (
     <div className="nominations-wrapper">
